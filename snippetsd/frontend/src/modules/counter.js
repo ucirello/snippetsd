82c22e5ce617de672b43d3c100f347db3f9cb188
Carlos C <uldericofilho@gmail.com>
2018-06-10 02:54:53 +0200

    snippetsd: react boilerplate

---
+++ snippetsd/frontend/src/modules/counter.js
@@ -0,0 +1,109 @@
+// Copyright 2018 github.com/ucirello
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export const INCREMENT_REQUESTED = 'counter/INCREMENT_REQUESTED'
+export const INCREMENT = 'counter/INCREMENT'
+export const DECREMENT_REQUESTED = 'counter/DECREMENT_REQUESTED'
+export const DECREMENT = 'counter/DECREMENT'
+
+const initialState = {
+  count: 0,
+  isIncrementing: false,
+  isDecrementing: false
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT_REQUESTED:
+      return {
+        ...state,
+        isIncrementing: true
+      }
+
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+        isIncrementing: !state.isIncrementing
+      }
+
+    case DECREMENT_REQUESTED:
+      return {
+        ...state,
+        isDecrementing: true
+      }
+
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+        isDecrementing: !state.isDecrementing
+      }
+
+    default:
+      return state
+  }
+}
+
+export const increment = () => {
+  return dispatch => {
+    dispatch({
+      type: INCREMENT_REQUESTED
+    })
+
+    dispatch({
+      type: INCREMENT
+    })
+  };
+}
+
+export const incrementAsync = () => {
+  return dispatch => {
+    dispatch({
+      type: INCREMENT_REQUESTED
+    })
+
+    return setTimeout(() => {
+      dispatch({
+        type: INCREMENT
+      })
+    }, 3000)
+  };
+}
+
+export const decrement = () => {
+  return dispatch => {
+    dispatch({
+      type: DECREMENT_REQUESTED
+    })
+
+    dispatch({
+      type: DECREMENT
+    })
+  };
+}
+
+export const decrementAsync = () => {
+  return dispatch => {
+    dispatch({
+      type: DECREMENT_REQUESTED
+    })
+
+    return setTimeout(() => {
+      dispatch({
+        type: DECREMENT
+      })
+    }, 3000)
+  };
+}
