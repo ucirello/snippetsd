b8a465147f526163f85b01d5e96a2ac1cdadb3f0
Carlos C <uldericofilho@gmail.com>
2018-06-12 02:36:26 +0200

    snippetsd: Add error package

---
+++ snippetsd/pkg/errors/debug_stub.go
@@ -0,0 +1,16 @@
+// Based on upspin.io/errors
+
+// +build !debug
+
+package errors
+
+import "bytes"
+
+// These are stubs that disable stack collecting/printing
+// when the debug build tag is not set.
+// For documentation about these types and functions, see debug.go.
+
+type stack struct{}
+
+func (e *Error) populateStack()           {}
+func (e *Error) printStack(*bytes.Buffer) {}
